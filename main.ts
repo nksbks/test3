5ebc28701cb0a4ee384554c414ba87a3d06c6ed8
nksbks <121257584+nksbks@users.noreply.github.com>
2023-01-15 18:32:39 +0200

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # . #
+        . # # # .
+        # . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . # . .
+        . # # # .
+        . . # . #
+        . # . # .
+        . . # . .
+        `)
+})
